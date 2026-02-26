9148de01dcc37de25c7ffc4462cbd0865df09df2
Matias DEV <colmatias.desarrollo@gmail.com>
2026-02-26 10:16:51 -0300

    config tablas

--- src/Config/config.js
+++ src/Config/config.js
@@ -21,7 +21,7 @@
      * URL del Web App de Google Apps Script (despliegue). ÚNICA fuente de datos de la app.
      * Copiar aquí la URL que da "Implementar" > "Aplicación web" en el proyecto Apps Script vinculado al Sheet anterior.
      */
-    APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzPhmQ3sm3k1Pomy_hPkeqkx4epG1xWN9rgd6Mv1win7CulaEsFNvU54lwqrV3eOglX9A/exec',
+    APP_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx2rDJyiiUfM-M0seU6ejBmcM36NdzXxMaxLNIH9CeP8OC2if37Rk4FVI08Sak2tYohpw/exec',
 
     /** Nombres de las hojas (igual que en Code.gs y tables.js). */
     HOJA_PRODUCTOS: 'PRODUCTOS',
